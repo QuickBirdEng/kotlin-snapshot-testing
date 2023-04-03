a76676b93f90a7c866bf8ccbcc6f00c3a2ccb3d4
Balazs Toth <balazs.toth@quickbirdstudios.com>
2023-04-03 15:07:06 +0200

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
-        classpath("com.android.tools.build:gradle:7.2.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.android.tools.build:gradle:7.4.2")
     }
 }
 
